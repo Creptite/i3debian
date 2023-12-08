6271b6fb119dbbc5b444fc56ba806c26b8c92425
Creptite <64784798+Creptite@users.noreply.github.com>
2023-12-08 18:34:51 -0500

    Add files via upload

--- i3install.sh
+++ i3install.sh
@@ -2,65 +2,22 @@
 sudo apt install -y nala
 
 # Install essential applications
-sudo nala install -y \
-    timeshift \
-    lf \
-    thunar \
-    firefox-esr \
-    i3wm \
-    i3blocks \
-    rofi \
-    htop \
-    neovim \
-    vim \
-    git \
-#    tlp \
-#    tlp-rdw \
-    redshift \
-    xorg \
-    xblacklight \
-    xbindkeys \
-    xvkbd \
-    xinput \
-    xorg-dev \
-    python3-pip \
-    intel-microcode \
-    network-manager-gnome \
-    xfce4-settings \
-    xfce4-power-manager \
-    kitty \
-    pulseaudio \
-    alsa-utils \
-    pavucontrol \
-    volumeicon-alsa \
-    lxappearance \
-    dialog \
-    mtools \
-    dosfstools \
-    avahi-daemon \
-    acpi \
-    acpid \
-    gvfs-backends \
-    neofetch \
-    htop \
-    nitrogen \
-    fonts-font-awesome \
-    python3-i3ipc \
-    xfce4-screenshooter \
-    libreoffice \
-    gimp \
-    gparted \
-    thunderbird \
-    xdotool \
-    xdo \
-    hexchat \
-    zathura \
-    newsboat \
-    cmus \
+sudo nala install -y nala timeshift lf thunar firefox-esr i3 i3blocks rofi htop neovim vim git redshift xorg xbacklight xbindkeys xvkbd xinput xorg-dev python3-pip intel-microcode network-manager-gnome xfce4-settings xfce4-power-manager pulseaudio alsa-utils pavucontrol volumeicon-alsa lxappearance dialog mtools dosfstools avahi-daemon acpi acpid gvfs-backends neofetch htop nitrogen fonts-font-awesome python3-i3ipc xfce4-screenshooter libreoffice gimp gparted thunderbird xdotool xdo hexchat zathura newsboat qbittorrent xfce4-terminal cmus fonts-symbola thunar-archive-plugin neomutt mpv flameshot zsh curl network-manager-openvpn ttf-mscorefonts-installer
+#tlp tlp-rdw tp-smapi-dkms
+    #enable flatpaks
+    sudo apt install flatpak 
+    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    flatpak install discord stremio spotify io.github.shiftey.Desktop
+#install spicetify
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
 #todo
 #1 setup redshift config creation
 #2 pull configs for github and replace existing for i3, i3bar, redshift, lf,and others
 #3setup script for if thinkpad install tlp tlp rdw and tp-smapi-dkms. from there create the file in /etc/default/tlp and enable tlp 
+#grabs default i3block scripts cause im lazy
+git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks
+cd !$
+cp config.example config
 #Create folders in user directory (eg. Documents,Downloads,etc.)
 xdg-user-dirs-update
 
@@ -79,11 +36,11 @@ sudo make install installsystemd
 sudo systemctl enable ly.service
 
 # setup autotiling up using https://github.com/nwg-piotr/autotiling 
-git clone https://github.com/nwg-piotr/autotiling
-cd autotiling-master/autotiling
-cp main.py autotiling
-chmod +x autotiling
-sudo cp autotiling /bin
+#git clone https://github.com/nwg-piotr/autotiling
+#cd autotiling-master/autotiling
+#cp main.py autotiling
+#chmod +x autotiling
+#sudo cp autotiling /bin
 
 
 # XSessions and i3.desktop
